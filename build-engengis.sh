10a2957a327b71322ab0df05bc9f6bba974a25c9
redmaner <jakevdputten@gmail.com>
2012-01-24 13:52:51 +0100

    Add engengis terminal

--- build-engengis.sh
+++ build-engengis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Copyright (c) 2012, Jake "redmaner" van der Putten
 # Engengis build tool
-# Version: 21012012
+# Version: 2301121804
 
 # System requirements
 # -----------------------------------------
@@ -11,13 +11,70 @@
 
 VERSION=`cat currentversion`
 
+check () {
 if [ -d build ]; then
      echo "Start building engengis..."
      sleep 1
+     echo
+     echo "Do you want to build with extra options?"
+     echo "[y/n]"
+     read extraoptionbuild
 else
      mkdir build
+     check;
 fi;
+
+case "$extraoptionbuild" in
+  "y" | "Y") terminal;;
+  "n" | "N") compile;;
+esac
+}
+
+terminal () {
+echo
+echo "Do you want to include engengis terminal?"
+echo "[y/n]"
+read includeterminal;
+
+case "$includeterminal" in
+  "y" | "Y")
+  cp extra/terminal include/system/etc/engengis/terminal;
+  settingsconfig;;
+  "n" | "N") settingsconfig;;
+esac
+}
+
+settingsconfig () {
+echo
+echo "Do you want to include pre-build settings.conf file?"
+echo "[y/n]"
+read includesettingsconfig;
+
+case "$includesettingsconfig" in
+  "y" | "Y")
+  cp extra/settings.conf include/data/settings.conf;
+  engengisconfig;;
+  "n" | "N") engengisconfig;;
+esac
+}
+
+engengisconfig () {
+echo
+echo "Do you want to include pre-build engengis.conf file?"
+echo "[y/n]"
+read includeengengisconfig;
+
+case "$includeengengisconfig" in
+  "y" | "Y")
+  cp extra/engengis.conf include/data/engengis.conf;
+  compile;;
+  "n" | "N") compile;;
+esac
+}
+
+compile () {
 cd include
+echo
 echo "Making zip archieve..."
 sleep 1
 zip -r engengis * 
@@ -33,3 +90,26 @@ echo "Done find build at:"
 echo "/build/$VERSION.zip"
 rm -f signzip/engengis.zip
 sleep 3
+echo
+echo "Do you want to keep extra options?"
+echo "[y/n]"
+read remove
+
+case "$remove" in
+  "y" | "Y") 
+  echo
+  echo "End off build progress"
+  sleep 2; exit;;
+  "n" | "N" )
+  rm -f include/system/etc/engengis/terminal
+  rm -f include/data/settings.conf
+  rm -f include/data/engengis.conf
+  echo
+  echo "Extra options removed"
+  echo
+  echo "End off build progress"
+  sleep 2; exit;;
+esac  
+}
+
+check;
